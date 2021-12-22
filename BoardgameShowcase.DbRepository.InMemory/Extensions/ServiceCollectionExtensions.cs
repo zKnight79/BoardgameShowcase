9fecde25d58c058914a759e58c6a07f735f31f98
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 22:38:08 +0100

    Started InMemory repositories

---
+++ BoardgameShowcase.DbRepository.InMemory/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BoardgameShowcase.DbRepository.Extensions;
+using BoardgameShowcase.DbRepository.InMemory.Repository;
+using BoardgameShowcase.DbRepository.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoardgameShowcase.DbRepository.InMemory.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBoardgameShowcaseDbRepositoryInMemory(this IServiceCollection services)
+        {
+            services.AddTransient<IAuthorRepository, AuthorRepository>();
+
+            services.AddBoardgameShowcaseDbRepository();
+
+            return services;
+        }
+    }
+}
---
+++ BoardgameShowcase.DbRepository.InMemory/Repository/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using BoardgameShowcase.Common;
+using BoardgameShowcase.Common.Extensions;
+using BoardgameShowcase.Common.Utility;
+using BoardgameShowcase.DbRepository.Repository;
+using BoardgameShowcase.Model.Entity;
+using Microsoft.Extensions.Logging;
+
+namespace BoardgameShowcase.DbRepository.InMemory.Repository
+{
+    abstract class GenericRepository<T> : Loggable<GenericRepository<T>>, IGenericRepository<T> where T : GenericEntity
+    {
+        protected static List<T> Entities { get; } = new();
+        
+        protected GenericRepository(ILogger<GenericRepository<T>> logger)
+            : base(logger)
+        {
+            
+        }
+
+        protected abstract T CloneEntity(T entity);
+        
+        public Task<IEnumerable<T>> FindAllAsync()
+        {
+            Logger.LogMethodCall();
+
+            List<T> entities = new();
+            foreach (T entity in Entities)
+            {
+                entities.Add(CloneEntity(entity));
+            }
+            
+            return Task.FromResult<IEnumerable<T>>(entities);
+        }
+
+        public Task<T?> FindByIdAsync(string entityId)
+        {
+            Logger.LogMethodCall(entityId);
+
+            T? entity = Entities.FirstOrDefault(x => x.Id == entityId);
+            if (entity is not null)
+            {
+                entity = CloneEntity(entity);
+            }
+
+            return Task.FromResult(entity);
+        }
+
+        public Task<string?> InsertAsync(T entity)
+        {
+            Logger.LogMethodCall(entity);
+
+            T? newEntity = null;
+            if (entity.IsNew)
+            {
+                newEntity = CloneEntity(entity);
+                newEntity.Id = StringUtil.NewGuid();
+                Entities.Add(newEntity);
+            }
+            
+            return Task.FromResult(newEntity?.Id);
+        }
+
+        public Task<bool> UpdateAsync(T entity)
+        {
+            Logger.LogMethodCall(entity);
+
+            bool updated = false;
+
+            if (!entity.IsNew)
+            {
+                int index = Entities.IndexOf(entity);
+                if (index >= 0)
+                {
+                    Entities[index] = CloneEntity(entity);
+                    updated = true;
+                }
+            }
+            
+            return Task.FromResult(updated);
+        }
+
+        public Task<bool> DeleteByIdAsync(string entityId)
+        {
+            Logger.LogMethodCall(entityId);
+
+            int removeCount = Entities.RemoveAll(e => e.Id == entityId);
+            bool deleted = removeCount > 0;
+
+            return Task.FromResult(deleted);
+        }
+    }
+}
