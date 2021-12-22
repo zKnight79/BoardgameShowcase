e50bf227dbbb921298b888fde6c00f8c88c6e6cd
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 14:14:59 +0100

    Added Common library project

---
+++ BoardgameShowcase.Common/Extensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace BoardgameShowcase.Common.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void SetValues<T>(this List<T> list, IEnumerable<T> values)
+        {
+            if (list != values)
+            {
+                list.Clear();
+                list.AddRange(values);
+            }
+        }
+    }
+}
