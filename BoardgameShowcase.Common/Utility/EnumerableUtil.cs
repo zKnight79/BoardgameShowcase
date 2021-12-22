fc18423eea1f753102bf775cf0438235ab9dfcab
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 15:33:26 +0100

    Added Loggable

---
+++ BoardgameShowcase.Common/Utility/EnumerableUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace BoardgameShowcase.Common.Utility
+{
+    public static class EnumerableUtil
+    {
+        public static IEnumerable<T> From<T>(params T?[] args)
+        {
+            return from arg in args
+                   where arg is not null
+                   select arg;
+        }
+    }
+}
