944740fe1155d2d98596c12dee704fe7a7f2361f
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 19:01:16 +0100

    Added Dependency Injection

---
+++ BoardgameShowcase.DbRepository/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BoardgameShowcase.DbRepository.Service;
+using BoardgameShowcase.Model.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoardgameShowcase.DbRepository.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBoardgameShowcaseDbRepository(this IServiceCollection services)
+        {
+            services.AddTransient<IAuthorService, AuthorService>();
+            services.AddTransient<IIllustratorService, IllustratorService>();
+            services.AddTransient<IPublisherService, PublisherService>();
+            services.AddTransient<IBoardgameService, BoardgameService>();
+            
+            return services;
+        }
+    }
+}
