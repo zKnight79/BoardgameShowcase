d4a994f4cea9cfccf5bea0df68f1eca88aed72d5
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 16:57:00 +0100

    Added DbRepository library project

---
+++ BoardgameShowcase.DbRepository/Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using BoardgameShowcase.Model.Entity;
+
+namespace BoardgameShowcase.DbRepository.Repository
+{
+    public interface IGenericRepository<T> where T : GenericEntity
+    {
+        Task<IEnumerable<T>> FindAllAsync();
+        Task<T?> FindByIdAsync(string entityId);
+        Task<string?> InsertAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteByIdAsync(string entityId);
+    }
+}
---
+++ BoardgameShowcase.DbRepository/Service/IllustratorService.cs
@@ -0,0 +1,26 @@
+﻿using BoardgameShowcase.Common.Extensions;
+using BoardgameShowcase.DbRepository.Repository;
+using BoardgameShowcase.Model.Entity;
+using BoardgameShowcase.Model.Service;
+using Microsoft.Extensions.Logging;
+
+namespace BoardgameShowcase.DbRepository.Service
+{
+    class IllustratorService : GenericService<Illustrator>, IIllustratorService
+    {
+        private readonly IIllustratorRepository _illustratorRepository;
+        
+        public IllustratorService(ILogger<IllustratorService> logger, IIllustratorRepository illustratorRepository)
+            : base(logger, illustratorRepository)
+        {
+            _illustratorRepository = illustratorRepository;
+        }
+
+        public Task<IEnumerable<Illustrator>> GetByNameAsync(string namePart)
+        {
+            Logger.LogMethodCall(namePart);
+
+            return _illustratorRepository.FindByNameAsync(namePart);
+        }
+    }
+}
