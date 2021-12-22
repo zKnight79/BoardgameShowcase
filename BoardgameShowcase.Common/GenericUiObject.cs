b3b47c1753165f8708161d97708399c63c9c0353
zKnight79 <guillaume.jibaut@live.com>
2021-12-22 15:41:47 +0100

    Added GenericUiObject

---
+++ BoardgameShowcase.Common/GenericUiObject.cs
@@ -0,0 +1,20 @@
+﻿using BoardgameShowcase.Common.Utility;
+
+namespace BoardgameShowcase.Common
+{
+    public abstract class GenericUiObject
+    {
+        public string UiId { get; } = StringUtil.NewGuid();
+
+        public override int GetHashCode()
+        {
+            return UiId.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is GenericUiObject uiObject
+                   && uiObject.GetType() == GetType()
+                   && uiObject.UiId == UiId;
+        }
+    }
+}
