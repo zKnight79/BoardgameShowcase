d20a63f6ad1419d9513a994e481ce5cc595ea46c
zKnight79 <guillaume.jibaut@live.com>
2021-12-26 16:29:31 +0100

    Extension method to call Private Unherited Parameterless Methods Returning a given type

---
+++ BoardgameShowcase.Common/Extensions/GenericExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace BoardgameShowcase.Common.Extensions
+{
+    public static class GenericExtensions
+    {
+        public static void CallPrivateUnheritedParameterlessMethodsReturning<TInstance>(this TInstance instance, Type returnType)
+        {
+            if (instance is not null)
+            {
+                Type objType = instance.GetType();
+                IEnumerable<MethodInfo> methods = objType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Where(
+                        x => x.DeclaringType == objType
+                        && x.IsPrivate
+                        && x.ReturnType == returnType
+                        && x.GetParameters().Length == 0
+                    );
+                foreach (MethodInfo method in methods)
+                {
+                    _ = method.Invoke(instance, null);
+                }
+            }
+        }
+    }
+}
