bd63104bcca13ecdf95c3b67fb44b431dc0b4d9d
zKnight79 <guillaume.jibaut@live.com>
2022-01-04 23:07:45 +0100

    Enhancing authors' table

---
+++ BoardgameShowcase.Portal/Shared/AuthorComponents/AuthorTableRow.razor.cs
@@ -0,0 +1,25 @@
+﻿using BoardgameShowcase.Model.Entity;
+using BoardgameShowcase.Model.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace BoardgameShowcase.Portal.Shared.AuthorComponents
+{
+    public partial class AuthorTableRow
+    {
+        private const int MAX_BOARDGAME_COUNT = 3;
+        
+        [Inject] private IBoardgameService BoardgameService { get; set; } = default!;
+
+        [Parameter, EditorRequired] public Author Author { get; set; } = default!;
+
+        private IEnumerable<Boardgame> Boardgames { get; set; } = Enumerable.Empty<Boardgame>();
+
+        private IEnumerable<Boardgame> BoardgameList => Boardgames.Take(MAX_BOARDGAME_COUNT);
+        private int BoardgameCount => Boardgames.Count();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Boardgames = await BoardgameService.GetByAuthorAsync(Author);
+        }
+    }
+}
