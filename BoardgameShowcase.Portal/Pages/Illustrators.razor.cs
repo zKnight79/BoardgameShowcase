c1479ccefa887a0ecc728adcc6ec5550279ec514
zKnight79 <guillaume.jibaut@live.com>
2022-01-06 14:11:25 +0100

    Added Illustrators page

---
+++ BoardgameShowcase.Portal/Pages/Illustrators.razor.cs
@@ -0,0 +1,20 @@
+﻿using BoardgameShowcase.Model.Entity;
+using BoardgameShowcase.Model.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace BoardgameShowcase.Portal.Pages
+{
+    public partial class Illustrators
+    {
+        public const string ROUTE = "illustrators";
+
+        [Inject] private IIllustratorService IllustratorService { get; set; } = default!;
+
+        private IEnumerable<Illustrator> IllustratorList { get; set; } = Enumerable.Empty<Illustrator>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            IllustratorList = await IllustratorService.GetAllAsync();
+        }
+    }
+}
