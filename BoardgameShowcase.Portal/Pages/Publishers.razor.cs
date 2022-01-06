0d78e03aa1a4482987ff621e19ccab6b90251b84
zKnight79 <guillaume.jibaut@live.com>
2022-01-06 15:25:32 +0100

    Added Publishers page

---
+++ BoardgameShowcase.Portal/Pages/Publishers.razor.cs
@@ -0,0 +1,20 @@
+﻿using BoardgameShowcase.Model.Entity;
+using BoardgameShowcase.Model.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace BoardgameShowcase.Portal.Pages
+{
+    public partial class Publishers
+    {
+        public const string ROUTE = "publishers";
+
+        [Inject] private IPublisherService PublisherService { get; set; } = default!;
+
+        private IEnumerable<Publisher> PublisherList { get; set; } = Enumerable.Empty<Publisher>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            PublisherList = await PublisherService.GetAllAsync();
+        }
+    }
+}
---
+++ BoardgameShowcase.Portal/Pages/PublishersTableRow.razor.cs
@@ -0,0 +1,25 @@
+﻿using BoardgameShowcase.Model.Entity;
+using BoardgameShowcase.Model.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace BoardgameShowcase.Portal.Pages
+{
+    public partial class PublishersTableRow
+    {
+        private const int MAX_BOARDGAME_COUNT = 3;
+
+        [Inject] private IBoardgameService BoardgameService { get; set; } = default!;
+
+        [Parameter, EditorRequired] public Publisher Publisher { get; set; } = default!;
+
+        private IEnumerable<Boardgame> Boardgames { get; set; } = Enumerable.Empty<Boardgame>();
+
+        private IEnumerable<Boardgame> BoardgameList => Boardgames.Take(MAX_BOARDGAME_COUNT);
+        private int BoardgameCount => Boardgames.Count();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Boardgames = await BoardgameService.GetByPublisherAsync(Publisher);
+        }
+    }
+}
