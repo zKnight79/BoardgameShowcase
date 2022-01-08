b03b2d4ecf860a857f1498070008fbd6e85dd452
zKnight79 <guillaume.jibaut@live.com>
2022-01-08 23:08:57 +0100

    Theming the portal

--- BoardgameShowcase.Portal/TailwindCSS/tailwind.config.js
+++ BoardgameShowcase.Portal/TailwindCSS/tailwind.config.js
@@ -1,10 +1,33 @@
+function withOpacityValue(colorVar) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${colorVar}))`;
+    }
+    return `rgb(var(${colorVar}) / ${opacityValue})`;
+  }
+}
+
 module.exports = {
   content: [
     "../Pages/**/*.{cshtml,razor}",
     "../Shared/**/*.{cshtml,razor}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': withOpacityValue('--bgsc-background'),
+        'on-background': withOpacityValue('--bgsc-on-background'),
+        'primary': withOpacityValue('--bgsc-primary'),
+        'on-primary': withOpacityValue('--bgsc-on-primary'),
+        'secondary': withOpacityValue('--bgsc-secondary'),
+        'on-secondary': withOpacityValue('--bgsc-on-secondary'),
+        'menu': {
+          'top': withOpacityValue('--bgsc-menu-top'),
+          'bottom': withOpacityValue('--bgsc-menu-bottom'),
+          'text': withOpacityValue('--bgsc-menu-text'),
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
