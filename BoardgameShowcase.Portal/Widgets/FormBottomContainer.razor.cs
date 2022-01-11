1e4675c1a1272e356506c7f0bc79ec3b9b734e97
zKnight79 <guillaume.jibaut@live.com>
2022-01-11 11:39:55 +0100

    Added FormBottomContainer

---
+++ BoardgameShowcase.Portal/Widgets/FormBottomContainer.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BoardgameShowcase.Portal.Widgets
+{
+    public partial class FormBottomContainer
+    {
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+    }
+}
