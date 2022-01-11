df9e32d6f3dfcc65ee0e4adb2e925fa33ef72250
zKnight79 <guillaume.jibaut@live.com>
2022-01-11 11:05:39 +0100

    FormTextField refactoring

--- BoardgameShowcase.Portal/Widgets/FormTextField.razor.cs
+++ BoardgameShowcase.Portal/Widgets/FormTextField.razor.cs
@@ -4,22 +4,22 @@ namespace BoardgameShowcase.Portal.Widgets
 {
     public partial class FormTextField
     {
-        [Parameter] public string FieldValue { get; set; } = default!;
-        [Parameter] public EventCallback<string> FieldValueChanged { get; set; }
+        [Parameter] public string Value { get; set; } = default!;
+        [Parameter] public EventCallback<string> ValueChanged { get; set; }
         [Parameter] public string FieldLabel { get; set; } = string.Empty;
         [Parameter] public string FieldPlaceholder { get; set; } = string.Empty;
         [Parameter] public string InvalidityHint { get; set; } = string.Empty;
         [Parameter] public bool IsRequired { get; set; } = false;
         [Parameter] public bool IsReadonly { get; set; } = false;
+        [Parameter] public bool SelectOnFocus { get; set; } = false;
 
         private ElementReference InputField { get; set; }
 
         public async Task InputValueChanged(ChangeEventArgs eventArgs)
         {
-            string? newValue = eventArgs?.Value as string;
-            if (newValue is not null)
+            if (eventArgs.Value is string newValue)
             {
-                await FieldValueChanged.InvokeAsync(newValue);
+                await ValueChanged.InvokeAsync(newValue);
             }
         }
 
