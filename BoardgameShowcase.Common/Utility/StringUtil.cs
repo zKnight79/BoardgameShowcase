cbfc91aece1a4971b4b292c17aa8f8e0fda1f4e7
zKnight79 <guillaume.jibaut@live.com>
2022-01-11 09:37:42 +0100

    Added StringUtil.JoinNonNullOrWhiteSpace method

--- BoardgameShowcase.Common/Utility/StringUtil.cs
+++ BoardgameShowcase.Common/Utility/StringUtil.cs
@@ -10,5 +10,14 @@
         /// </summary>
         /// <returns>A new GUID.</returns>
         public static string NewGuid() => $"{Guid.NewGuid():N}";
+
+        /// <summary>
+        /// Generate a <see cref="string"/> by separating non null <see cref="string"/> or white space arguments with a separator.
+        /// </summary>
+        /// <param name="separator">The separator used to separate the  <see cref="string"/> arguments.</param>
+        /// <param name="args">The  <see cref="string"/> arguments.</param>
+        /// <returns>The newly generated <see cref="string"/>.</returns>
+        public static string JoinNonNullOrWhiteSpace(string separator, params string?[] args)
+            => string.Join(separator, args.Where(str => !string.IsNullOrWhiteSpace(str)));
     }
 }
